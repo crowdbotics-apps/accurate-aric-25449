b66064835fc0ba0b409728b0fcc6c9667c0e192b
Crowdbotics <team@crowdbotics.com>
2021-04-03 23:43:45 +0000

    Update models

--- backend/accurate_aric_25449/wsgi.py
+++ backend/accurate_aric_25449/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'accurate_aric_25449.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "accurate_aric_25449.settings")
 
 application = get_wsgi_application()
